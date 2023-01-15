426e81cc1904f38791065cc555120fec07bb62a0
FloreDenis2001 <floredenis907@yahoo.com>
2023-01-15 06:22:13 +0200

    master

---
+++ src/main/java/ro/mycode/managemusicportal/ManageMusicPortalApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.managemusicportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageMusicPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageMusicPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/managemusicportal/ManageMusicPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.managemusicportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageMusicPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
